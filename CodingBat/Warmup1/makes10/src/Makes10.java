59788ae7f0eefb309db560f261056aba5950cc5a
mjeshwanth <jeshwanthmalluru@gmail.com>
2024-09-30 22:03:25 +0530

    makes10

---
+++ CodingBat/Warmup1/makes10/src/Makes10.java
@@ -0,0 +1,10 @@
+public class Makes10 {
+    public static void main(String[] args){
+              Makes10 obj =new Makes10();
+                System.out.println(obj.Makes10(9,10));
+    }
+    public boolean Makes10(int a, int b) {
+  return (a==10 || b==10 || a+b == 10);
+}
+}
+
---
+++ CodingBat/Warmup1/makes10/src/obj.java
@@ -0,0 +1,15 @@
+/*
+My Java Project.
+ */
+
+/**
+ *
+ * @author jeshw_3agky5x
+ */
+class obj {
+
+    static boolean Makes10(int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
